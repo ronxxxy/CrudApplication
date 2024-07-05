9ad356478b9771f47a3e4e5b623e5af1310f4cfc
Abdul Rafay <mughalrafay999@gmail.com>
2024-07-05 17:47:36 +0500

    Add project files.

---
+++ CrudApplication/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using CrudApplication.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudApplication.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext (DbContextOptions options) : base (options)
+        {
+            
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
